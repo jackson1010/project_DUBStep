fb75851a0bf9414f12e83c5a26f019be03638ed2
cyeow <cheryl.ylt@gmail.com>
2024-01-31 02:27:51 +0800

    added report card component

---
+++ healthhack-ai-tools/src/pages/ReportPage/ReportPage.jsx
@@ -0,0 +1,31 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+import ReportItem from "./ReportItem";
+
+const reports = [
+    {
+        title: 'Report 1',
+        date: '21 Dec 2023',
+        comments: 'Lalalalaa',
+    },
+    {
+        title: 'Report 2',
+        date: '1 Jun 2021',
+        comments: 'Hahahahaaaa',
+    },
+    {
+        title: 'Report 3',
+        date: '30 Oct 2020',
+        comments: 'Babababaaa',
+    },
+]
+const ReportPage = () => {
+    return (
+        <Container maxW={"container.lg"}>
+            {reports.map(report => {
+                return (<ReportItem report={report} />);
+            })}
+        </Container>
+    );
+};
+
+export default ReportPage;
