22d468d5b53ec89671ed679b3030ab4e6b9122c8
vincentkua <zylyn89@gmail.com>
2024-02-02 22:58:06 +0800

    feat: added show details logic

--- healthhack-ai-tools/src/pages/ReportPage/ReportItem.jsx
+++ healthhack-ai-tools/src/pages/ReportPage/ReportItem.jsx
@@ -1,34 +1,58 @@
-import { Card, CardBody, CardHeader, Heading, Button, Text, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Button,
+  Text,
+  Flex,
+  Spacer,
+} from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const defaultReport = {
-    title: 'Lung X Ray',
-    date: '24 Jan 2023',
-    comments: 'Some doctor\'s commment here',
-}
+  title: "Lung X Ray",
+  date: "24 Jan 2023",
+  comments: "Some doctor's commment here",
+};
 
 const ReportItem = (_props) => {
-    const report = _props.report || defaultReport;
+  const report = _props.report || defaultReport;
+  const [showDetails, setShowDetails] = useState(false);
 
-    return (
-        <div style={{marginTop: '2rem'}}>
-            <Card>
-                <CardHeader>
-                    <Flex>
-                        <div>
-                            <Heading size='md'>{report.title}</Heading>
-                            <Text>{report.date}</Text>
-                        </div>
-                        <Spacer />
-                        <Button variant='outline'>More details &nbsp;<ChevronRightIcon /></Button>
-                    </Flex>
-                </CardHeader>
-                <CardBody>
-                    <Text>{report.comments}</Text>
-                </CardBody>
-            </Card>
-        </div>
-    );
+  const toggleShowDetails = () => {
+    showDetails ? setShowDetails(false) : setShowDetails(true);
+  };
+
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <Card>
+        <CardHeader>
+          <Flex>
+            <div>
+              <Heading size="md">{report.title}</Heading>
+              <Text>{report.date}</Text>
+            </div>
+            <Spacer />
+            <Button variant="outline" onClick={toggleShowDetails}>
+              {showDetails ? "Hide details" : "More details"} &nbsp;
+              <ChevronRightIcon />
+            </Button>
+          </Flex>
+        </CardHeader>
+        <CardBody paddingTop={"0"}>
+          <Text>{report.comments}</Text>
+          {showDetails && (
+            <>
+              <br />
+              <Text>Details ....</Text>
+            </>
+          )}
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default ReportItem;
