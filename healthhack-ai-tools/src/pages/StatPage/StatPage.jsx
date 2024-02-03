b765320672d5be438cb60c4940010b025be7a684
code_tofu <benjamin.ftc@gmail.com>
2024-02-03 15:40:21 +0800

    added stat page

--- healthhack-ai-tools/src/pages/StatPage/StatPage.jsx
+++ healthhack-ai-tools/src/pages/StatPage/StatPage.jsx
@@ -1,34 +1,41 @@
-import { Container } from "@chakra-ui/react";
+import { Container,Heading } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { BarGraph } from "../../components/Graph/BarGraph";
 import { testGraphData, testGraphOptions } from "../../../data/testGraphData";
+import StatItem  from "./StatItem"
+
+const metrics = [{
+  "id":1,
+  "sysBp": 140,
+  "diaBp" : 90,
+  "chol" : 5.5,
+  "exerciseTime" : 80
+}]
+
 
 const StatPage = () => {
-  const [fitbitRecords, setFitbitRecords] = useState([]);
+  const [fitbitRecords, setFitbitRecords] = useState(metrics);
 
-  useEffect(() => {
-    // Fetch data from the Fitbit API or any other external health record provider
-    fetch("api/fitbitRecords") // Replace 'api/fitbitRecords' with the actual API endpoint
-      .then((response) => response.json())
-      .then((data) => setFitbitRecords(data))
-      .catch((error) => console.error("Error fetching Fitbit records:", error));
-  }, []);
+  // useEffect(() => {
+  //   // Fetch data from the Fitbit API or any other external health record provider
+  //   fetch("api/fitbitRecords") // Replace 'api/fitbitRecords' with the actual API endpoint
+  //     .then((response) => response.json())
+  //     .then((data) => setFitbitRecords(data))
+  //     .catch((error) => console.error("Error fetching Fitbit records:", error));
+  // }, []);
+
 
   return (
     <>
-      <Container>
-      <h1>External Health Records</h1>
-      <ul>
-        {fitbitRecords.map((record) => (
-          <li key={record.id}>
-            <p>Date: {record.date}</p>
-            <p>Steps: {record.steps}</p>
-            {/* Add more details based on Fitbit or other app data */}
-          </li>
-        ))}
-      </ul>
+      <Container maxW={"container.lg"}>
+      <Heading as='h2'>External Health Records</Heading>
+      <Container maxW={"container.lg"}>
+      {fitbitRecords.map((record) => (
+        <StatItem key={record.id} record={record} />
+      ))}
+    </Container>
       </Container>
-      <Container maxW='3xl' position="relative" centerContent>
+      <Container maxW={"container.lg"} position="relative" centerContent>
         <BarGraph chartData={testGraphData} chartOptions={testGraphOptions}/>
       </Container>
     </>
